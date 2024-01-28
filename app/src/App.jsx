ed68240cb6387b0f96809af89d275e0ea0bfc912
Vishnuprasanth-2002 <s.vishnuprasanth14@gmail.com>
2024-01-28 12:41:25 +0530

    tweety project

---
+++ app/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import Home from "./Components/Home";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
